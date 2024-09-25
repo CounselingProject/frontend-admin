eabf7f8f138c36c072208d76bacbe9673bcabeca
lionjeongin <cindy93489163@gmail.com>
2024-09-25 11:22:45 +0900

    게시판 페이지 구현

--- src/app/board/update/[bid]/page.js
+++ src/app/board/update/[bid]/page.js
@@ -1,6 +1,7 @@
 import UpdateContainer from '@/board/containers/UpdateContainer';
 import AdminOnlyContainer from '@/member/containers/AdminOnlyContainer';
 const BoardUpdatePage = ({ params }) => {
+  
   return (
     <AdminOnlyContainer>
       <UpdateContainer params={params} />
