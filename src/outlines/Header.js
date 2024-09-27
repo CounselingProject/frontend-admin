16af12c03d824d1f7f7a5b9b772b43b0292efb79
parksaehyun <psh666777@naver.com>
2024-09-27 10:38:13 +0900

    style : 로그인, 회원가입 페이지, 헤더 부분

--- src/outlines/Header.js
+++ src/outlines/Header.js
@@ -1,29 +1,52 @@
+'use client';
 import React, { useCallback, useState, useEffect } from 'react';
 import styled from 'styled-components';
 import cookies from 'react-cookies';
 import { useTranslation } from 'react-i18next';
 import { getCommonStates } from '../commons/contexts/CommonContext';
 import { getUserContext } from '@/commons/contexts/UserInfoContext';
+import Image from 'next/image';
+
 const HeaderBox = styled.header`
-  .site-top {
-    background: ${({ theme }) => theme.colors.white};
-    height: 65px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+    background: ${({ theme }) => theme.colors.green};
+    color: ${({ theme }) => theme.colors.white};
 
-    div {
-      text-align: right;
+    .logo-container {
+        display: flex;
+        align-items: center;
 
-      a {
-        display: inline-block;
-        line-height: 34px;
-        margin-left: 10px;
-        font-size: ${({ theme }) => theme.fontSizes.normal};
-
-        &.on {
-          color: ${({ theme }) => theme.colors.green};
+        .logo {
+            margin-right: 10px; /* 두 로고 사이 간격 */
         }
-      }
     }
-  }
+
+    .main-menu {
+        display: flex;
+        margin-right: auto; /* 메뉴를 왼쪽으로 정렬 */
+        a {
+            margin: 0 15px;
+            color: ${({ theme }) => theme.colors.white};
+            text-decoration: none;
+
+            &:hover {
+                text-decoration: underline;
+            }
+        }
+    }
+
+    .auth-links {
+        display: flex;
+        align-items: center;
+
+        a {
+            margin-left: 10px;
+            color: ${({ theme }) => theme.colors.white};
+        }
+    }
 `;
 
 const Header = () => {
@@ -46,27 +69,52 @@ const Header = () => {
     cookies.remove('token', { path: '/' });
   }, [setIsLogin, setIsAdmin, setUserInfo]);
 
+  // 관리자 URL
+  const adminUrl =
+    process.env.NEXT_PUBLIC_ADMIN_URL + '?token=' + cookies.load('token');
+
   return (
     showHeader && (
       <HeaderBox>
-        <section className="site-top">
-          <div className="layout-width">
-            {login ? (
-              <>
-                {/* 로그인 상태 */}
-                <span>
-                  {userInfo?.userName}({userInfo?.email}){t('님_로그인')}
-                </span>
-                <span onClick={onLogout}>{t('로그아웃')}</span>
-              </>
-            ) : (
-              <>
-                <a href="/member/join">{t('회원가입')}</a>
-                <a href="/member/login">{t('로그인')}</a>
-              </>
-            )}
-          </div>
-        </section>
+        <div className="logo-container">
+          <a href="/" className="logo">
+            <Image
+              src="/images/logo/logo1.png"
+              alt="로고1"
+              width={43}
+              height={35}
+            />
+          </a>
+          <a href="/" className="logo">
+            <Image
+              src="/images/logo/logo3.png"
+              alt="로고2"
+              width={200}
+              height={10}
+            />
+          </a>
+        </div>
+        <div className="auth-links">
+          {login ? (
+            <>
+              <span>
+                {userInfo?.userName}({userInfo?.email}){t('님_로그인')}
+              </span>
+              <a onClick={onLogout}>{t('로그아웃')}</a>
+              <a href="/mypage">{t('마이페이지')}</a>
+              {isAdmin && (
+                <a href={adminUrl} target="_blank">
+                  {t('사이트_관리')}
+                </a>
+              )}
+            </>
+          ) : (
+            <>
+              <a href="/member/join">{t('회원가입')}</a>
+              <a href="/member/login">{t('로그인')}</a>
+            </>
+          )}
+        </div>
       </HeaderBox>
     )
   );
